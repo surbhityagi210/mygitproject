563b65e9e72860fac59a69b05e9a936b4246d2a9
tyagi.s <tyagi.s@DT-GANGWARA.Dataworld.co.in>
2018-06-18 15:27:29 +0530

    test commit

---
+++ src/test/java/MavenSeleniumSample/demoforMaven.java
@@ -0,0 +1,11 @@
+package MavenSeleniumSample;
+
+import org.testng.annotations.Test;
+
+public class demoforMaven {
+	@Test
+	public void testHello()
+	{
+		System.out.println("Welcome to Maven world");
+	}
+}
